46642d763a77dec115f1ff1ac13be8f7ca9b4646
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2022-03-26 10:46:24 +0300

    More

--- stringFunction/Source.c
+++ stringFunction/Source.c
@@ -7,6 +7,10 @@ char* myStrCopy(char *ptr1, char *ptr2);
 char* myStrnCopy(char* ptr1, char* ptr2, int );
 void myStrCat(char* ptr, char* ptr1);
 void myStrnCat(char s1[], char s2[], int a);
+void myStrCmp(char* a, char* b);
+void myStrnCmp(char* a, char* b);
+void myStrnCmp(char* a, char* b, int x);
+
 
 int main()
 {
@@ -18,6 +22,9 @@ int main()
 	char s2[] = { "New year " };
 	char s3[40] = { "" };
 	
+	char x1[] = "abed";
+	char x2[] = "abcd";
+	char x3[] = "Happy Holidays";
 
 	/*printf("%s\n", myStrCopy(b, a));
 	printf("%s\n",myStrnCopy(b, a, 3));*/
@@ -25,9 +32,12 @@ int main()
 	/*myStrCat(s1, s2);
 	printf("%s", s1);*/
 	
-	myStrnCat(s1, s2,4);
-	printf("%s", s1);
+	/*myStrnCat(s1, s2,4);
+	printf("%s", s1);*/
 
+	/*myStrCmp(x2, x1);*/
+	
+	myStrnCmp(x2, x1, 3);
 }
 
 int strln(char *ptr1)
@@ -82,4 +92,65 @@ void myStrnCat( char s1[], char s2[], int a)
 		s1[k + i] = s2[i];
 	}
 	/*s1[a + k] = '\0';*/
+}
+
+void myStrCmp(char* a,char* b)
+{
+	int flag = 0;
+	int k = strln(b);
+	int n = strln(a);
+	int big;
+	if (k>n)
+	{
+		big = k;
+	}
+	else
+	{
+		big = n;
+	}
+	for (int i = 0; i < big; i++)
+	{
+		if (a[i] < b[i])
+		{
+			printf("-1");
+			flag = 1;
+			break;
+		}
+		else if(a[i] > b[i])
+		{
+			printf("1");
+			flag = 1;
+			break;
+		}
+	}
+	if (flag==0)
+	{
+		printf("0");
+	}
+
+}
+
+void myStrnCmp(char* a, char* b,int x)
+{
+	int flag = 0;
+	
+	for (int i = 0; i < x; i++)
+	{
+		if (a[i] < b[i])
+		{
+			printf("-1");
+			flag = 1;
+			break;
+		}
+		else if (a[i] > b[i])
+		{
+			printf("1");
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0)
+	{
+		printf("0");
+	}
 }
